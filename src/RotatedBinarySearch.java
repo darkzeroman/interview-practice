7ce46751535ec6ee65d798640c4d44cac794a137
Vidhur Vohra <dkzjunk@yahoo.com>
2013-03-17 14:14:58 -0400

    Adding rotating binary search.

--- src/RotatedBinarySearch.java
+++ src/RotatedBinarySearch.java
@@ -2,6 +2,7 @@ public class RotatedBinarySearch {
 
 	public static void main(String[] args) {
 		System.out.println(recCall(new int[] { 4, 5, 1, 2, 3 }, 0, 4, 4));
+		System.out.println(binSearch(new int[] { 1, 2, 3, 4, 5 }, 0, 4, 5));
 	}
 
 	/** Binary search for an element in a rotated array */
@@ -21,5 +22,19 @@ public class RotatedBinarySearch {
 			else
 				return recCall(arr, left, mid - 1, target);
 		}
+	}
+
+	public static int binSearch(int[] arr, int left, int right, int target) {
+		int mid = left + (right - left) / 2;
+
+		if (arr[mid] == target) {
+			return mid;
+		} else if (target < arr[mid]) {
+			return binSearch(arr, left, mid - 1, target);
+
+		} else { // right half
+			return binSearch(arr, mid + 1, right, target);
+		}
+
 	}
 }
